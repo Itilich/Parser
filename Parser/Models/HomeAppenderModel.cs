29fb4d8d983079180cd8952c4026c9e045a4b827
ecattrina <katy_060104@mail.ru>
2024-12-16 22:23:34 +0500

    Редактирование страницы Appender и модели

--- Parser/Models/HomeAppenderModel.cs
+++ Parser/Models/HomeAppenderModel.cs
@@ -4,7 +4,7 @@ namespace Parser.Models
 {
     public class HomeAppenderModel
     {
-        public string Name { get; set; }
+        public string ProductName { get; set; }
         public string LinkCersanit { get; set; }
         public string LinkVodoparad { get; set; }
     }
