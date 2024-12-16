99bc1ee811e8fc90e68004077e9a17f9aa7c30f7
Itilich <aaperlovka@gmail.com>
2024-12-16 19:29:00 +0500

    Добавьте файлы проекта.

---
+++ Parser/Data/ParserContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Parser.Data
+{
+    public class ParserContext: DbContext
+    {
+
+        public ParserContext(DbContextOptions<ParserContext> options): base (options) 
+        { 
+        }
+
+    }
+}
