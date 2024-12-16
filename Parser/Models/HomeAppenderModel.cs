0a3a600ce74c37c9ddf814b73a60827e003535b7
Itilich <aaperlovka@gmail.com>
2024-12-16 21:10:18 +0500

    Добавлена новая модель HomeAppenderModel

---
+++ Parser/Models/HomeAppenderModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Policy;
+
+namespace Parser.Models
+{
+    public class HomeAppenderModel
+    {
+        public string Name { get; set; }
+        public string LinkCersanit { get; set; }
+        public string LinkVodoparad { get; set; }
+    }
+}
